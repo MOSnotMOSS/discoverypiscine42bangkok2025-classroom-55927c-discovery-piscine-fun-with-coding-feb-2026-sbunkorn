d8a715e91433ac19c69514a356c3ee81fd3ac231
Saponthon <112682294+Saponthon@users.noreply.github.com>
2026-02-06 22:21:13 +0700

    add

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+count=0
+for arg in "$@"; do
+    echo "$arg"
+    count=$((count + 1))
+    if [ $count -eq 3 ]; then
+        break
+    fi
+done
